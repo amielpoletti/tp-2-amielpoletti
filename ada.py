e075add760ab1abf554ff4df7fc5ab7c98233dd8
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-03-22 12:37:57 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(first_name.lower() + " " + last_name.lower())
+    print(first_name.lower().title() + " " + last_name.lower().title())
+    print(first_name.upper() + " " + last_name.upper())
+    tabulado = f"\t{first_name.lower()} {last_name.lower()}"
+    print(tabulado)
+
+ada()
